75e2c008aa711254e25b0ce5aa34ed9392148e0c
Milan Justel <milancjustel@gmail.com>
2025-12-02 21:43:00 -0800

    Refactored day 3 to share logic

--- src/day3.rs
+++ src/day3.rs
@@ -9,7 +9,7 @@ fn part_1(contents: &str) -> usize {
 
     battery_banks
         .into_iter()
-        .map(|bank| calculate_joltage(&bank))
+        .map(|bank| calculate_joltage(&bank, 2))
         .sum()
 }
 
@@ -24,47 +24,23 @@ fn parse_banks(contents: &str) -> Vec<Vec<usize>> {
         .collect()
 }
 
-fn calculate_joltage(battery_bank: &Vec<usize>) -> usize {
-    let max_digit = battery_bank[0..(battery_bank.len() - 1)]
-        .iter()
-        .max()
-        .unwrap();
-
-    let mut current_max = 0;
-    for i in 0..(battery_bank.len() - 1) {
-        if battery_bank[i] != *max_digit {
-            continue;
-        }
-
-        let second_max_digit = battery_bank[(i + 1)..].iter().max().unwrap();
-
-        let max = max_digit * 10 + second_max_digit;
-
-        if max > current_max {
-            current_max = max;
-        }
-    }
-
-    current_max
-}
-
 fn part_2(contents: &str) -> usize {
     let battery_banks = parse_banks(contents);
 
     battery_banks
         .into_iter()
-        .map(|bank| calculate_joltage_v2(&bank))
+        .map(|bank| calculate_joltage(&bank, 12))
         .sum()
 }
 
-fn calculate_joltage_v2(battery_bank: &Vec<usize>) -> usize {
+fn calculate_joltage(battery_bank: &Vec<usize>, num_batteries: usize) -> usize {
     let mut best_max = 0;
 
-    calculate_joltage_v2_helper(
+    calculate_joltage_helper(
         battery_bank,
         0,
-        battery_bank.len() - 11,
-        0,
+        battery_bank.len() - (num_batteries - 1),
+        num_batteries,
         0,
         &mut best_max,
     );
@@ -72,15 +48,15 @@ fn calculate_joltage_v2(battery_bank: &Vec<usize>) -> usize {
     best_max
 }
 
-fn calculate_joltage_v2_helper(
+fn calculate_joltage_helper(
     battery_bank: &Vec<usize>,
     start: usize,
     end: usize,
-    current_digits: usize,
+    remaining_batteries: usize,
     current_val: usize,
     best: &mut usize,
 ) {
-    if current_digits == 12 {
+    if remaining_batteries == 0 {
         if current_val > *best {
             *best = current_val;
         }
@@ -88,21 +64,21 @@ fn calculate_joltage_v2_helper(
         return;
     }
 
-    let max_digit = battery_bank[start..end].iter().max().unwrap();
+    let max_battery = battery_bank[start..end].iter().max().unwrap();
 
-    let current = current_val * 10 + max_digit;
+    let current_val = current_val * 10 + max_battery;
 
     for i in start..end {
-        if battery_bank[i] != *max_digit {
+        if battery_bank[i] != *max_battery {
             continue;
         }
 
-        calculate_joltage_v2_helper(
+        calculate_joltage_helper(
             battery_bank,
             i + 1,
             end + 1,
-            current_digits + 1,
-            current,
+            remaining_batteries - 1,
+            current_val,
             best,
         );
     }
