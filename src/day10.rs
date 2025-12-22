81ab5a7ea7d837d944388f0152a568173e508374
Milan Justel <milancjustel@gmail.com>
2025-12-22 11:40:28 -0800

    Completed day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -13,7 +13,7 @@ pub fn run() {
 struct Machine {
     lights: usize,
     buttons: Vec<usize>,
-    joltage: Vec<usize>,
+    joltages: Vec<usize>,
 }
 
 impl Machine {
@@ -35,7 +35,7 @@ impl Machine {
             .collect();
 
         let joltage = pieces[pieces.len() - 1];
-        let joltage = joltage[1..joltage.len() - 1]
+        let joltages = joltage[1..joltage.len() - 1]
             .split(",")
             .map(|s| s.parse::<usize>().unwrap())
             .collect();
@@ -43,7 +43,7 @@ impl Machine {
         Self {
             lights,
             buttons,
-            joltage,
+            joltages,
         }
     }
 
@@ -78,9 +78,7 @@ fn calculate_presses(machine: Machine) -> usize {
             return num_pushes;
         }
 
-        if let Some(best_count) = presses.get(&state)
-            && *best_count <= num_pushes
-        {
+        if let Some(best_count) = presses.get(&state) && *best_count <= num_pushes {
             continue;
         }
 
@@ -95,7 +93,102 @@ fn calculate_presses(machine: Machine) -> usize {
 }
 
 fn part_2(contents: &str) -> usize {
-    0
+    let machines = parse_machines(contents);
+
+    machines.into_iter()
+        .map(calculate_presses_joltage_v3)
+        .sum()
+}
+
+fn calculate_presses_joltage_v3(machine: Machine) -> usize {
+    // From the subreddit: https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+
+    let button_cache = build_button_cache(&machine.buttons);
+    let mut cache = HashMap::new();
+    calculate_presses_joltage_helper(&button_cache, &machine.joltages, &mut cache).unwrap()
+}
+
+fn build_lights(joltages: &Vec<usize>) -> usize {
+    joltages.iter().rev().fold(0, |acc, x| (acc << 1) + (x % 2))
+}
+
+fn build_button_cache(buttons: &Vec<usize>) -> HashMap<usize, Vec<Vec<usize>>> {
+    let mut button_cache: HashMap<usize, Vec<Vec<usize>>> = HashMap::new();
+
+    for i in 0..=((1 << buttons.len()) - 1) {
+        let mut mask = i;
+        let mut index = 0;
+        let mut button_group = vec![];
+        while mask != 0 {
+            if mask & 1 != 0 {
+                button_group.push(buttons[index]);
+            }
+
+            mask >>= 1;
+            index += 1;
+        }
+
+        let target = button_group.iter().fold(0, |state, x| state ^ x);
+
+        button_cache.entry(target).or_default().push(button_group);
+    }
+
+    button_cache
+}
+
+fn apply_buttons(buttons: &Vec<usize>, joltages: &Vec<usize>) -> Option<Vec<usize>> {
+    let mut joltages = joltages.clone();
+
+    for button in buttons {
+        let mut mask = *button;
+        let mut index = 0;
+        while mask != 0 {
+            if mask & 1 != 0 {
+                if joltages[index] == 0 {
+                    return None;
+                }
+
+                joltages[index] -= 1;
+            }
+
+            mask >>= 1;
+            index += 1;
+        }
+    }
+
+    joltages.iter_mut().for_each(|x| *x /= 2);
+
+    Some(joltages)
+}
+
+fn calculate_presses_joltage_helper(button_cache: &HashMap<usize, Vec<Vec<usize>>>, joltages: &Vec<usize>, cache: &mut HashMap<Vec<usize>, Option<usize>>) -> Option<usize>
+{
+    if joltages.iter().all(|x| *x == 0) {
+        return Some(0);
+    }
+
+    if let Some(best) = cache.get(joltages) {
+        return *best;
+    }
+
+    let target = build_lights(joltages);
+
+    let mut best = None;
+    if let Some(possible_buttons) = button_cache.get(&target) {
+        for button_group in possible_buttons {
+            let child_best = apply_buttons(button_group, joltages)
+                .and_then(|new_joltages| calculate_presses_joltage_helper(button_cache, &new_joltages, cache))
+                .and_then(|val| Some(val * 2 + button_group.len()));
+
+            if best.is_none_or(|best| child_best.is_some_and(|child_best| child_best < best)) {
+                best = child_best;
+            }
+        }
+    }
+
+    cache.insert(joltages.clone(), best);
+
+    best
 }
 
 #[cfg(test)]
@@ -120,13 +213,13 @@ mod tests {
     fn test_example_part_2() {
         let contents = utilities::read_file_data(DAY, "example.txt");
 
-        assert_eq!(part_2(&contents), 0);
+        assert_eq!(part_2(&contents), 33);
     }
 
     #[test]
     fn test_input_part_2() {
         let contents = utilities::read_file_data(DAY, "input.txt");
 
-        assert_eq!(part_2(&contents), 0);
+        assert_eq!(part_2(&contents), 17820);
     }
 }
